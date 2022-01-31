901a5ddb06e3aa196fe18c8f500899fc42725537
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2022-01-31 21:10:59 +0100

    Errore

---
+++ classes/tipi-prodotti.php
@@ -0,0 +1,11 @@
+<?php
+
+    class TipiProdotto extends Utenti {
+        private $tipo;
+
+        public function setTipo($_tipo){
+            $this->tipo = $_tipo;
+        }
+    }
+
+?>
--- index.php
+++ index.php
@@ -1,6 +1,24 @@
 <?php
-    require_once __DIR__.'/classes/prodotti.php';
     require_once __DIR__.'/classes/utenti.php';
+    require_once __DIR__.'/classes/prodotti.php';
+    require_once __DIR__.'/classes/tipi-prodotti.php';
+
+    $utenti = [
+        new Utenti("Michael", "Cash", "M"),
+        new Utenti("Sara", "Banda", "F"),
+        new Utenti("Guido", "Piano", "M"),
+    ];
+
+    $prodotti = [
+        'FruttaVerdura' => new Prodotto("Mela", 1),
+        'Pesce' => new Prodotto("Salmone", 9),
+    ];
+    $prodotti['FruttaVerdura'] = new TipiProdotto;
+    $prodotti['FruttaVerdura']->setTipo("Frutta");
+    $prodotti['Pesce'] = new TipiProdotto;
+    $prodotti['Pesce']->setTipo("Pesce");
+
+    var_dump($utenti);
 ?>
 
 <!DOCTYPE html>
